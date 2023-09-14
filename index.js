60599afdb1a92191edc1c762ef9e928a8d6d21cd
Marian <mocanumarian1993@gmail.com>
2023-09-14 19:38:42 +0200

    minor changes

--- index.js
+++ index.js
@@ -1,29 +1,34 @@
-const app = require("express")();
+const express = require("express");
 const { connectToDatabase, closeDatabaseConnection, db } = require("./database");
+const { ObjectId } = require("mongodb");
+const cors = require("cors");
+
+const app = express();
 const port = 3000;
-const bodyParser = require("body-parser");
 
-app.use(bodyParser.json());
+app.use(cors({ origin: "*", methods: ["GET", "POST"] }));
+app.use(express.json());
 
-app.get("/", async (req, res) => {
+app.get("/travel-destinations", async (req, res) => {
   try {
     const destinations = await db.collection("destinations").find().toArray();
-    res.status(200).json({ destinations });
+    res.status(200).json(destinations);
   } catch (error) {
     console.error("Error reading collection", error);
     res.status(500).json({ error: "Internal server error occured" });
   }
 });
 
-app.post("/", async (req, res) => {
+app.post("/travel-destination", async (req, res) => {
   try {
-    if (!req.body.title || !req.body.country || !req.body.link) {
+    if (!req.body.title || !req.body.country) {
       res.status(400).json({ error: "Missing required fields" });
       return;
     } else {
       const newDestination = req.body;
       const result = await db.collection("destinations").insertOne(newDestination);
-      res.status(201).json({ message: "Document added sucessfully", insertedId: result.insertedId });
+      const foundDestination = await db.collection("destinations").findOne({ _id: new ObjectId(result.insertedId) });
+      res.status(201).json(foundDestination);
     }
   } catch (error) {
     console.error("Error adding document", error);
