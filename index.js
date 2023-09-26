e0430b1dca4ed5e1c4d53b94528fc0453a851f5b
Marian <mocanumarian1993@gmail.com>
2023-09-26 11:25:46 +0200

    added update and delete functionalities

--- index.js
+++ index.js
@@ -6,15 +6,37 @@ const cors = require("cors");
 const app = express();
 const port = 3000;
 
-app.use(cors({ origin: "*", methods: ["GET", "POST"] }));
+app.use(cors({ origin: "*" }));
 app.use(express.json());
+
+const notDeletedFilter = {
+  $or: [
+    { deleted: { $exists: false } }, // Documents where the "deleted" field does not exist
+    { deleted: false }, // Documents where the "deleted" field is explicitly set to false
+  ],
+};
 
 // get all travel destinations
 app.get("/travel-destinations", async (req, res) => {
-  res.setHeader("Content-Type", "application/javascript");
   try {
-    const destinations = await db.collection("destinations").find().toArray();
+    const destinations = await db.collection("destinations").find(notDeletedFilter).toArray();
     res.status(200).json(destinations);
+  } catch (error) {
+    console.error("Error reading collection", error);
+    res.status(500).json({ error: "Internal server error occured" });
+  }
+});
+
+// get a specific travel destination
+app.get("/travel-destinations/:id", async (req, res) => {
+  const filter = { _id: new ObjectId(req.params.id), ...notDeletedFilter };
+  try {
+    const foundDestination = await db.collection("destinations").findOne(filter);
+    if (foundDestination) {
+      res.status(200).json(foundDestination);
+    } else {
+      res.status(404).json({ error: "No document matched the query" });
+    }
   } catch (error) {
     console.error("Error reading collection", error);
     res.status(500).json({ error: "Internal server error occured" });
@@ -23,14 +45,14 @@ app.get("/travel-destinations", async (req, res) => {
 
 // create a travel destination
 app.post("/travel-destinations", async (req, res) => {
-  res.setHeader("Content-Type", "application/javascript");
   try {
     if (!req.body.title || !req.body.country) {
       res.status(400).json({ error: "Missing required fields" });
     } else {
       const newDestination = req.body;
       const result = await db.collection("destinations").insertOne(newDestination);
-      const foundDestination = await db.collection("destinations").findOne({ _id: new ObjectId(result.insertedId) });
+      const filter = { _id: new ObjectId(result.insertedId) };
+      const foundDestination = await db.collection("destinations").findOne(filter);
       res.status(201).json(foundDestination);
     }
   } catch (error) {
@@ -39,14 +61,43 @@ app.post("/travel-destinations", async (req, res) => {
   }
 });
 
-// update a travel destination
-app.put("/travel-destination/:id", async (req, res) => {
-  res.setHeader("Content-Type", "application/javascript");
+// update a specific travel destination
+app.put("/travel-destinations/:id", async (req, res) => {
+  const filter = { _id: new ObjectId(req.params.id), ...notDeletedFilter };
   try {
-    const result = await db.collection("destinations").findOne({ _id: ObjectId(req.params.id) });
-    res.status(200).json(result);
+    const foundDestination = await db.collection("destinations").findOne(filter);
+    const updatedDestination = { ...foundDestination, ...req.body };
+    const result = await db.collection("destinations").replaceOne(filter, updatedDestination);
+
+    // FIXME: improve success condition ???
+    if (result.modifiedCount === 1) {
+      res.status(200).json(updatedDestination);
+    } else if (result.matchedCount === 0) {
+      res.status(404).json({ error: "No document matched the query, so no update occured" });
+    } else {
+      res.status(200).json({ response: "Update did not occur or had no effect" });
+    }
   } catch (error) {
     console.error("Error updating document", error);
+    res.status(500).json({ error: "Internal server error occured" });
+  }
+});
+
+// delete a specific travel destination
+app.delete("/travel-destinations/:id", async (req, res) => {
+  const filter = { _id: new ObjectId(req.params.id) };
+  const update = { $set: { deleted: true } };
+  try {
+    const result = await db.collection("destinations").updateOne(filter, update);
+    if (result.modifiedCount === 1) {
+      res.status(200).json({ deletedId: req.params.id });
+    } else if (result.matchedCount === 0) {
+      res.status(404).json({ error: "No document matched the query, so no deletion occured" });
+    } else {
+      res.status(200).json({ deletedId: req.params.id });
+    }
+  } catch (error) {
+    console.error("Error deleting document", error);
     res.status(500).json({ error: "Internal server error occured" });
   }
 });
